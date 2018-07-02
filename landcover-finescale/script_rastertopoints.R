c7bb1c2343941d57c4581ae4625979bfb42c84d0
Di Cecco <gdicecco@ad.unc.edu>
2018-07-02 14:13:33 -0400

    add script to convert rasters to data tables

---
+++ landcover-finescale/script_rastertopoints.R
@@ -0,0 +1,25 @@
+## Extract land cover values as tables from rasters of BCRs
+
+#### Libraries ####
+library(raster)
+library(stringr)
+
+# 2001-2011
+setwd("/proj/hurlbertlab/gdicecco/nlcd_frag_proj_shapefiles/BCRs_landcover_output/")
+files <- list.files()
+nlcd.files <- files[str_detect(files, ".grd")]
+dir.grd <- getwd()
+nlcd.names <- str_split(nlcd.files, ".grd", 1)
+
+for(i in 1:length(nlcd.files)){
+  file <- nlcd.files[i]
+  name <- nlcd.names[[i]]
+  
+  raster <- raster(paste0(dir.grd, "/", file))
+  
+  df <- as.data.frame(rasterToPoints(raster))
+  colnames(df)[3] <- "code"
+  write.csv(df, paste0(name, ".csv"), row.names = F)
+  
+  print(paste0("Completed file ", file))
+}
