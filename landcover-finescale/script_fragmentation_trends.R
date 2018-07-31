0f482f8f2a1848d8fcfccfa6ccfb776bc1ffdbb7
Di Cecco <gdicecco@ad.unc.edu>
2018-07-31 11:35:54 -0400

    add other time windows

--- landcover-finescale/script_fragmentation_trends.R
+++ landcover-finescale/script_fragmentation_trends.R
@@ -3,9 +3,25 @@ library(SDMTools)
 library(raster)
 library(stringr)
 
+# 2001-2006
 setwd("/proj/hurlbertlab/gdicecco/nlcd_frag_proj_shapefiles/BCRs_landcover_output/01-06/")
 
 files <- list.files(pattern = "2006_bcr_")
+files.grd <- files[grepl("grd", files)]
+wd <- "/proj/hurlbertlab/gdicecco/nlcd_frag_proj_shapefiles/BCRs_landcover_output/csvs/"
+
+for(i in 7:length(files.grd)) {
+  filename <- files.grd[i]
+  nameid <- word(filename, sep = "\\.")[1]
+  data <- raster(filename)
+  fragstats <- ClassStat(data, cellsize = 30, bkgd = 0)
+  write.csv(fragstats, paste0(wd, nameid, "_frag.csv"), row.names = F)
+}
+
+# 2006-2011
+setwd("/proj/hurlbertlab/gdicecco/nlcd_frag_proj_shapefiles/BCRs_landcover_output/06-11/")
+
+files <- list.files(pattern = "2011_bcr_")
 files.grd <- files[grepl("grd", files)]
 wd <- "/proj/hurlbertlab/gdicecco/nlcd_frag_proj_shapefiles/BCRs_landcover_output/csvs/"
 
@@ -16,3 +32,19 @@ for(i in 1:length(files.grd)) {
   fragstats <- ClassStat(data, cellsize = 30, bkgd = 0)
   write.csv(fragstats, paste0(wd, nameid, "_frag.csv"), row.names = F)
 }
+
+# 1992-2001
+setwd("/proj/hurlbertlab/gdicecco/nlcd_frag_proj_shapefiles/BCRs_landcover_output/92-01/")
+
+files <- list.files(pattern = "2001_bcr_")
+files.grd <- files[grepl("grd", files)]
+wd <- "/proj/hurlbertlab/gdicecco/nlcd_frag_proj_shapefiles/BCRs_landcover_output/csvs/"
+
+for(i in 1:length(files.grd)) {
+  filename <- files.grd[i]
+  nameid <- word(filename, sep = "\\.")[1]
+  data <- raster(filename)
+  fragstats <- ClassStat(data, cellsize = 30, bkgd = 0)
+  write.csv(fragstats, paste0(wd, nameid, "_frag.csv"), row.names = F)
+}
+
