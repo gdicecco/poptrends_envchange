44f2acb108b36fb54eb98994e966c6325fc33778
Grace Di Cecco <31256832+gdicecco@users.noreply.github.com>
2018-08-14 14:47:04 -0400

    Update for only remaining raster files

--- landcover-finescale/script_fragmentation_trends.R
+++ landcover-finescale/script_fragmentation_trends.R
@@ -4,34 +4,34 @@ library(raster)
 library(stringr)
 
 # 2001-2006
-setwd("/proj/hurlbertlab/gdicecco/nlcd_frag_proj_shapefiles/BCRs_landcover_output/01-06/")
+#setwd("/proj/hurlbertlab/gdicecco/nlcd_frag_proj_shapefiles/BCRs_landcover_output/01-06/")
 
-files <- list.files(pattern = "2006_bcr_")
-files.grd <- files[grepl("grd", files)]
-wd <- "/proj/hurlbertlab/gdicecco/nlcd_frag_proj_shapefiles/BCRs_landcover_output/csvs/"
+#files <- list.files(pattern = "2006_bcr_")
+#files.grd <- files[grepl("grd", files)]
+#wd <- "/proj/hurlbertlab/gdicecco/nlcd_frag_proj_shapefiles/BCRs_landcover_output/csvs/"
 
-for(i in 7:length(files.grd)) {
-  filename <- files.grd[i]
-  nameid <- word(filename, sep = "\\.")[1]
-  data <- raster(filename)
-  fragstats <- ClassStat(data, cellsize = 30, bkgd = 0)
-  write.csv(fragstats, paste0(wd, nameid, "_frag.csv"), row.names = F)
-}
+#for(i in 7:length(files.grd)) {
+#  filename <- files.grd[i]
+#  nameid <- word(filename, sep = "\\.")[1]
+#  data <- raster(filename)
+#  fragstats <- ClassStat(data, cellsize = 30, bkgd = 0)
+#  write.csv(fragstats, paste0(wd, nameid, "_frag.csv"), row.names = F)
+#}
 
 # 2006-2011
-setwd("/proj/hurlbertlab/gdicecco/nlcd_frag_proj_shapefiles/BCRs_landcover_output/06-11/")
+#setwd("/proj/hurlbertlab/gdicecco/nlcd_frag_proj_shapefiles/BCRs_landcover_output/06-11/")
 
-files <- list.files(pattern = "2011_bcr_")
-files.grd <- files[grepl("grd", files)]
-wd <- "/proj/hurlbertlab/gdicecco/nlcd_frag_proj_shapefiles/BCRs_landcover_output/csvs/"
+#files <- list.files(pattern = "2011_bcr_")
+#files.grd <- files[grepl("grd", files)]
+#wd <- "/proj/hurlbertlab/gdicecco/nlcd_frag_proj_shapefiles/BCRs_landcover_output/csvs/"
 
-for(i in 1:length(files.grd)) {
-  filename <- files.grd[i]
-  nameid <- word(filename, sep = "\\.")[1]
-  data <- raster(filename)
-  fragstats <- ClassStat(data, cellsize = 30, bkgd = 0)
-  write.csv(fragstats, paste0(wd, nameid, "_frag.csv"), row.names = F)
-}
+#for(i in 1:length(files.grd)) {
+#  filename <- files.grd[i]
+#  nameid <- word(filename, sep = "\\.")[1]
+#  data <- raster(filename)
+#  fragstats <- ClassStat(data, cellsize = 30, bkgd = 0)
+#  write.csv(fragstats, paste0(wd, nameid, "_frag.csv"), row.names = F)
+#}
 
 # 1992-2001
 setwd("/proj/hurlbertlab/gdicecco/nlcd_frag_proj_shapefiles/BCRs_landcover_output/92-01/")
@@ -40,7 +40,7 @@ files <- list.files(pattern = "2001_bcr_")
 files.grd <- files[grepl("grd", files)]
 wd <- "/proj/hurlbertlab/gdicecco/nlcd_frag_proj_shapefiles/BCRs_landcover_output/csvs/"
 
-for(i in 1:length(files.grd)) {
+for(i in 3:length(files.grd)) {
   filename <- files.grd[i]
   nameid <- word(filename, sep = "\\.")[1]
   data <- raster(filename)
