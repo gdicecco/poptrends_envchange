3ed8f6b55d5dedb89d0a7653fd91b1390d2cb1b6
Grace Di Cecco <31256832+gdicecco@users.noreply.github.com>
2018-08-31 16:27:15 -0400

    Remove excess lines of code

--- landcover-finescale/script_nlcd_finescale.R
+++ landcover-finescale/script_nlcd_finescale.R
@@ -11,18 +11,6 @@ library(stringr)
 #### Functions ####
 # Get area changeproduct raster file from NLCD directory
 
-# 1992-2001
-#setwd("nlcd_1992_to_2001_landcover_change")
-#files <- list.files()
-#area.files <- files[str_detect(files, "area_")]
-#dir <- getwd()
-
-#get.file <- function(x) {
-#  files2 <- list.files(paste0(dir, "/", area.files[x], ""))
-#  file.path <- files2[str_detect(files2, "area")]
-#  return(list(folder = area.files[x], file.name = file.path))
-#}
-
 # 2001-2011
 #setwd("/proj/hurlbertlab/nlcd_landcover_change/")
 #files <- list.files()
@@ -35,21 +23,8 @@ library(stringr)
 #  return(list(folder = nlcd.files[x], file.name = file.path))
 #}
 
-# Function to merge two raster files with different origins and extents, same resolution and crs
-#merge.areas <- function(x, y) {
-#  extent1 <- extent(x)
-#  extent2 <- extent(y)
-#  z <- raster(xmn = min(extent1[1], extent2[1]), xmx = max(extent1[2], extent2[2]), # make blank raster file with extent of the two files added
-#              ymn = min(extent1[3], extent2[3]), ymx = max(extent1[4], extent2[4]), 
-#              resolution = res(x), crs = crs(x))
-#  merge1 <- mosaic(z, x, fun = max, tolerance = max(abs(origin(z) - origin(x)))) # function: where rasters overlap keep larger value
-#  merge2 <- mosaic(merge1, y, fun = max, tolerance = max(abs(origin(merge1) - origin(y)))) # tolerance allows for different origins 
-#  return(merge2) # returns the two rasters merged into one
-#}
-
 #### Data needed for all time steps ####
-bcrs <- c(19, 23, 27, 29) # BCRs of interest
-# 9, 12, 13, 14, 18
+bcrs <- c(9, 12, 13, 14, 18, 19, 23, 27, 29) # BCRs of interest
 
 # BCR shapefile
 setwd("/proj/hurlbertlab/gdicecco/nlcd_frag_proj_shapefiles/BCRs_contiguous_us/")
@@ -61,14 +36,8 @@ us.proj <- readOGR("BCRs_contiguous_us.shp")
 setwd("/proj/hurlbertlab/gdicecco/nlcd_frag_proj_shapefiles/")
 region <- raster("nlcd_1992_2001_changepixels_30m_30m.grd")
 
-#crs.nlcd <- crs(region)
-#bcrs.proj <- spTransform(us.proj, crs.nlcd)
-
-#setwd("/proj/hurlbertlab/gdicecco/NLCD_fragmentation/landcover-finescale/")
-#codes <- read.csv("anderson_land_cover_codes.csv", stringsAsFactors = F) # NLCD land cover class codes
-
-#fragcodes <- codes %>%
-#  filter(grepl("Forest|Grassland", anderson))
+crs.nlcd <- crs(region)
+bcrs.proj <- spTransform(us.proj, crs.nlcd)
 
 # output: raster file of landcover change
 # output: dataframe of landcover change of interest
@@ -80,8 +49,6 @@ for(i in 1:length(bcrs)) {
   zones.sub <- extract(area.sub, bcr.sub, buffer = 200)
   filename <- paste0("nlcd_30x30_1992_2001_bcr_", bcr, ".grd")
   writeRaster(zones.sub, filename = filename)
-#  area.df <- rasterToPoints(zones.sub, fun = fun(x) {x %in% fragcodes$modified})
-#  write.csv(area.df, paste0("nlcd_30x30_1992_2001_bcr_", bcr, ".csv"), row.names = F)
 }
 
 #### 2001 - 2011 ####
@@ -95,10 +62,6 @@ for(i in 1:length(bcrs)) {
 # BCRs
 #us01 <- sp::spTransform(us.proj, crs(nlcd2001))
 
-# Land cover class codes
-#setwd("/proj/hurlbertlab/gdicecco/NLCD_fragmentation/landcover-finescale/")
-#codes0111 <- read.csv("nlcd_2001-2011_landcover_change_codes.csv", stringsAsFactors = F)
-
 # output: raster file of landcover change
 # output: dataframe of landcover change of interest
 #setwd("/proj/hurlbertlab/gdicecco/nlcd_frag_proj_shapefiles/BCRs_landcover_output/")
@@ -110,8 +73,6 @@ for(i in 1:length(bcrs)) {
  # zones.sub <- mask(area.sub, bcr.sub)
  # filename <- paste0("nlcd_30x30_2001_2006_bcr_", bcr, ".grd")
  # writeRaster(zones.sub, filename = filename, overwrite = T)
- # area.df <- rasterToPoints(zones.sub, fun = function(x) {x %in% codes0111$ID})
- # write.csv(area.df, paste0("nlcd_30x30_2001_2006_bcr_", bcr, ".csv"), row.names = F)
 #}
 
 ## 2006-2011
@@ -131,6 +92,4 @@ for(i in 1:length(bcrs)) {
 #  zones.sub <- mask(area.sub, bcr.sub)
 #  filename <- paste0("nlcd_30x30_2006_2011_bcr_", bcr, ".grd")
 #  writeRaster(zones.sub, filename = filename, overwrite = T)
-#  area.df <- rasterToPoints(zones.sub, fun = function(x) {x %in% codes0111$ID})
-#  write.csv(area.df, paste0("nlcd_30x30_2006_2011_bcr_", bcr, ".csv"), row.names = F)
 #}
