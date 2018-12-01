0b59b99d5dfeb35b386c9076ec2d2acf4b2d1864
Grace Di Cecco <31256832+gdicecco@users.noreply.github.com>
2018-12-01 15:39:55 -0500

    fix

--- landcover-finescale/scripts_routes/script-fragmentation-trends-routes-11-subs.R
+++ landcover-finescale/scripts_routes/script-fragmentation-trends-routes-11-subs.R
@@ -25,8 +25,8 @@ for(i in 1:nrow(routes_tr@data)) {
   rte <- subset(routes_tr, rteno == routenos[i])
   rtenum <- routenos[i]
   nlcd_crop <- crop(nlcd, rte)
-  nlcd_mask <- mask(nlcd_crop, rtenum)
+  nlcd_mask <- mask(nlcd_crop, rte)
   class <- ClassStat(nlcd_mask)
-  filename <- paste0("classStat_nlcd_30x30_2011_route_", rte, ".csv")
+  filename <- paste0("classStat_nlcd_30x30_2011_route_", rtenum, ".csv")
   write.csv(class, filename, row.names = F)
 }
