d296eb7d756afe2c8cad2da47a5fc7b0fb76c70b
Di Cecco <gdicecco@ad.unc.edu>
2019-11-06 12:20:14 -0500

    Compare daymet and prism points in winter and summer

--- climate/daymet_investigation.R
+++ climate/daymet_investigation.R
@@ -82,28 +82,46 @@ files <- list.files("daymet/")
 daymet_nc <- nc_open(paste0("daymet/", files[1]))
 daymet_raster <- brick(paste0("daymet/", files[1]))
 
-daymet_may <- daymet_raster[[6]]
+crs(daymet_raster) <- "+proj=lcc +datum=WGS84 +lon_0=-100 +lat_0=42.5 +x_0=0 +y_0=0 +units=km +lat_1=25 +lat_2=60 +ellps=WGS84 +towgs84=0,0,0"
 
-daymet_bbs <- raster::extract(daymet_may, dplyr::select(routes, longitude, latitude), df = T)
 
-daymet_routes <- daymet_bbs %>% bind_cols(routes)
+daymet_june <- daymet_raster[[6]]
+daymet_january <- daymet_raster[[1]]
 
-names(daymet_routes)[2] <- "daymet_tmax"
+routes_sf <- dplyr::select(routes, longitude, latitude) %>%
+  st_as_sf(coords = c("longitude", "latitude")) %>%
+  st_set_crs(4236) %>%
+  st_transform("+proj=lcc +datum=WGS84 +lon_0=-100 +lat_0=42.5 +x_0=0 +y_0=0 +units=km +lat_1=25 +lat_2=60 +ellps=WGS84 +towgs84=0,0,0")
+
+daymet_bbs <- raster::extract(daymet_june, st_coordinates(routes_sf), df = T)
+daymet_bbs_jan <- raster::extract(daymet_january, st_coordinates(routes_sf), df = T)
+
+daymet_routes <- daymet_bbs %>% 
+  bind_cols(routes) %>%
+  left_join(daymet_bbs_jan)
+
+names(daymet_routes)[c(2,15)] <- c("daymet_tmax_jun", "daymet_tmax_jan")
 
 # PRISM
 
 options(prism.path = "C:/Users/gdicecco/Desktop/prism/")
-get_prism_monthlys("tmax", years = 2017, mon = c(6), keepZip = F)
+get_prism_monthlys("tmax", years = 2017, mon = c(1,6), keepZip = F)
 prism_files <- ls_prism_data()
 
-prism <- prism_stack(prism_files$files[[1]])
+routes_points <- dplyr::select(routes, route, longitude, latitude) %>%
+  st_as_sf(coords = c("longitude", "latitude"))
 
-prism_bbs <- raster::extract(prism, dplyr::select(daymet_routes, longitude, latitude), df = T) %>%
-  bind_cols(daymet_routes)
+prism <- prism_stack(prism_files$files[c(1,3)])
 
-names(prism_bbs)[2] <- "prism_tmax"
+prism_bbs <- raster::extract(prism, dplyr::select(daymet_routes, longitude, latitude), df = T) 
 
-plot(prism_bbs$prism_tmax, prism_bbs$daymet_tmax)
-abline(a = 0, b = 1)
+names(prism_bbs)[2:3] <- c("prism_tmax_jan", "prism_tmax_jun")
 
-cor(prism_bbs$prism_tmax, prism_bbs$daymet_tmax, use = "pairwise.complete.obs")
+tmax_compare <- daymet_routes %>%
+  left_join(prism_bbs)
+
+jan_cor <- cor(tmax_compare$prism_tmax_jan, tmax_compare$daymet_tmax_jan, use = "pairwise.complete.obs")
+jun_cor <- cor(tmax_compare$prism_tmax_jun, tmax_compare$daymet_tmax_jun, use = "pairwise.complete.obs")
+
+jan_plot <- ggplot(tmax_compare, aes(x = tmax_compare$daymet_tmax_jan, y = tmax_compare$prism_tmax_jan)) + 
+  geom_point() + geom_abline(slope = 1, intercept = 0) + theme_classic(base_size = 12)
