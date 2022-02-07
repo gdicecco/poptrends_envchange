575efbf8f06d66c52a6a733cc4296f719018497d
gdicecco <gdicecco@live.unc.edu>
2022-02-07 14:41:34 -0600

    longleaf params

--- model/bayes_mod.sh
+++ model/bayes_mod.sh
@@ -4,10 +4,10 @@
 #SBATCH --time=11-0
 #SBATCH -N 1
 #SBATCH -n 4
-#SBATCH --mem-per-cpu=10G
+#SBATCH --mem-per-cpu=50G
 
 # Load modules
-module load r/4.0.3
+module load r/4.1.0
 module load gcc/6.3.0
 
 # Run rscript
